0da96d52056f229aa9ec374a1789635494958b2e
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2018-10-19 11:55:44 +0200

    Add project files.

---
+++ WebAppMySQL/Models/MySqlDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;           //Added
+using MySql.Data.EntityFramework;   //Added
+
+namespace WebAppMySQL.Models
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))] //Added
+    public class MySqlDbContext : DbContext
+    {
+        public MySqlDbContext() : base ("MySqlPeople")
+        {
+
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
